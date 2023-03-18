b8fa2150de7f752f6e52023c0071aab1e608da0c
Kahlia Hogg <47726907+athletedecoded@users.noreply.github.com>
2023-03-18 19:48:36 +0000

    deployed lambda

--- README.md
+++ README.md
@@ -1,22 +1,20 @@
 # IDS721 Spring 2023 Project 3 - Rust Serverless with AWS
 
-Serverless S3 Backup using Lambda x Rust  
+Automated serverless S3 backup using AWS Lambda x Rust  
+
+![image](./assets/arch.png)
 
 
 ## What I Learnt
 
 * Deploying AWS serverless using Lambda x S3 trigger
-
+* Configuring S3 Access Points and Roles
 
 ## Developer Setup
 
-**Prequisites**
-1. S3 Buckets x 2 for input and output. Input bucket has access point.
-2. Lambda execution role 
+**Pre-Configure AWS**
 
-**Configure AWS IAM Role**
-
-1. Create an AWS IAM User policy with `AWSLambda_FullAccess`, `AmazonS3FullAccess` permissions and added custom permission config
+1. Create an IAM User policy for "LambdaDeveloper" with `AWSLambda_FullAccess` permissions and added custom inline permission config
 ```
 {
     "Version": "2012-10-17",
@@ -37,7 +35,7 @@ Serverless S3 Backup using Lambda x Rust
 }
 ```
 
-2. Create your ~/.aws/credentials file with environment variables: 
+2. Add "LambdaDeveloper" keys to your local ~/.aws/credentials file with environment variables: 
 * aws_access_key_id
 * aws_secret_access_key
 * aws_role_arn
@@ -45,36 +43,35 @@ Serverless S3 Backup using Lambda x Rust
 
 *Note: `aws_role_arn` is copied from the IAM user summary and is formatted as arn:aws:iam::<aws_acct>:user/<iam_user>*
 
-**Install Cargo Lambda into Virtual Env**
+3. Create an IAM Role policy for "S3xLambdaAccessRole" with `AmazonS3FullAccess` and `AWSLambdaBasicExecutionRole` permissions
+4. Allocate 3 x S3 Buckets (SourceBucket, OutputBucket, RubbishBucket) and add an Access Point to each
+
+**Deploy Lambda Function**
 
 ```
+# Install cargo lambda
 $ python3 -m venv ~/.venv
 $ source ~/.venv/bin/activate
 $ make install
-```
 
-**Build Binary**
-
-```
+# Build binary
 $ make release
-```
 
-**Deploy Lambda Fxn to AWS**
-```
+# Deploy to AWS
 $ make deploy
 ```
 
-## ToDos
+**AWS Permissions**
 
-- [ ] Deploy base serverless lambda function + S3 trigger for image resizing
-- [ ] Additional augmentations: greyscale, rotations
-- [ ] Unit testing (a la AWS Serverless Rust)
-- [ ] CI/CD with spinup/teardown (a la Datadog)
+1. From the AWS Lambda Console > Functions > s3-backup > Configuration > Permissions > change Execution role to preconfigured "S3xLambdaAccessRole"
+2. From the AWS Lambda Console > Functions > s3-backup > Configuration > Environment variables > set the following env variables coinciding with S3 Access Point ARNs: `SRC_AP_ARN`, `DEST_AP_ARN`, `RUBBISH_AP_ARN`
 
+*NB: Access Point ARNs will be of the format arn:aws:s3:<region>:<aws-id>:accesspoint/<access-point-name>*
 
 ## References
 
 * [Cargo Lambda for Rust](https://www.cargo-lambda.info/)
+* [Using the AWS SDK for Rust in AWS Lambda function](https://docs.aws.amazon.com/sdk-for-rust/latest/dg/lambda.html)
 * [AWS Lambda Events Docs for Rust](https://docs.rs/aws_lambda_events/latest/aws_lambda_events/index.html)
 * [AWS Serverless Rust Demo](https://github.com/aws-samples/serverless-rust-demo/)
 * [AWS Tutorial: S3 Object Lambda](https://aws.amazon.com/getting-started/hands-on/amazon-s3-object-lambda-to-dynamically-watermark-images/)
--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,108 @@
-use aws_lambda_events::event::s3::S3Event;use lambda_runtime::{run, service_fn, Error, LambdaEvent};
-use serde::{Serialize};
+use aws_lambda_events::event::s3::S3Event;
 use aws_sdk_s3::{
-    error::CopyObjectError, output::CopyObjectOutput, types::SdkError, Client
+    error::{CopyObjectError, DeleteObjectError},
+    output::{CopyObjectOutput, DeleteObjectOutput},
+    types::SdkError,
+    Client,
 };
+use lambda_runtime::{run, service_fn, Error, LambdaEvent};
+use serde::Serialize;
 use urlencoding::encode;
 
 #[derive(Serialize)]
 struct Response {
+    event_type: String,
     key: String,
-    msg: String
+    msg: String,
 }
 
+// Copy object from source bucket to destination bucket
 async fn cp_object(
     client: &Client,
-    src_bucket: &str,
-    dest_bucket: &str,
-    img_key: &str,
+    src_ap: &str,
+    dest_ap: &str,
+    obj_key: &str,
 ) -> Result<CopyObjectOutput, SdkError<CopyObjectError>> {
     // formatted per https://docs.rs/aws-sdk-s3/latest/aws_sdk_s3/client/struct.Client.html#method.copy_object
-    let copy_src = format!("{src_bucket}/object/{img_key}");
+    let copy_src = format!("{src_ap}/object/{obj_key}");
     println!("cp_src: {:?}", copy_src);
-    let cp_src_encoded = encode(&copy_src);
+    let copy_src_encoded = encode(&copy_src);
 
     client
         .copy_object()
-        .copy_source(cp_src_encoded)
-        .bucket(dest_bucket)
-        .key(img_key)
+        .copy_source(copy_src_encoded)
+        .bucket(dest_ap)
+        .key(obj_key)
         .send()
         .await
+}
+
+// Delete object from bucket
+async fn delete_object(
+    client: &Client,
+    bucket: &str,
+    key: &str,
+) -> Result<DeleteObjectOutput, SdkError<DeleteObjectError>> {
+    client.delete_object().bucket(bucket).key(key).send().await
 }
 
 /*
 AWS Lambda function which extracts info from S3 event and returns it as a JSON string.
  */
 async fn function_handler(event: LambdaEvent<S3Event>) -> Result<Response, Error> {
-    // get bucket name as string
-    // let bucket = event.payload.records[0].s3.bucket.name.as_ref().unwrap();
-    let src_bucket = "arn:aws:s3:us-east-2:141774272727:accesspoint/s3-backup-in-ap";
-    let dest_bucket = "arn:aws:s3:us-east-2:141774272727:accesspoint/s3-backup-out-ap";
-    let key = event.payload.records[0].s3.object.key.as_ref().unwrap();
-    // Use aws-sdk-s3 to get the object from S3
+    // Extract event info from S3 event
+    let event_type = event.payload.records[0]
+        .event_name
+        .as_ref()
+        .unwrap()
+        .as_str();
+    let key = event.payload.records[0]
+        .s3
+        .object
+        .key
+        .as_ref()
+        .unwrap()
+        .as_str();
+    // Load env variables
+    let src_ap = std::env::var("SRC_AP_ARN").expect("SRC_AP_ARN must be set");
+    let dest_ap = std::env::var("DEST_AP_ARN").expect("DEST_AP_ARN must be set");
+    let rubbish_ap = std::env::var("RUBBISH_AP_ARN").expect("RUBBISH_AP_ARN must be set");
+    // Configure client
     let config = aws_config::load_from_env().await;
     let client = Client::new(&config);
-    println!("{:?}", client);
-    // Copy object to destination bucket
-    let cp_resp = cp_object(&client, src_bucket, dest_bucket, key).await.unwrap();
-
-    let resp = Response {
-        key: format!("New object key: {key}"),
-        msg: format!("New object: {:?}", cp_resp)
-    };
-    // return `Response` (it will be serialized to JSON automatically by the runtime)
-    Ok(resp)
+    // match event type
+    match event_type {
+        // If event is ObjectCreated:Put or ObjectCreated:Post, copy object to destination bucket
+        "ObjectCreated:Put" | "ObjectCreated:Post" => {
+            println!("Copying object: {:?}", key);
+            let resp_msg = cp_object(&client, &src_ap, &dest_ap, key).await?;
+            return Ok(Response {
+                event_type: format!("Event trigger: {event_type}"),
+                key: format!("Object key: {key}"),
+                msg: format!("SUCCESS: {:?}", resp_msg),
+            });
+        }
+        // If event is ObjectRemoved:Delete, copy to rubbish then delete object from destination bucket
+        "ObjectRemoved:Delete" => {
+            println!("Moving object to rubbish: {:?}", key);
+            let _ = cp_object(&client, &dest_ap, &rubbish_ap, key).await?;
+            println!("Deleting object: {:?}", key);
+            let resp_msg = delete_object(&client, &dest_ap, key).await?;
+            return Ok(Response {
+                event_type: format!("Event trigger: {event_type}"),
+                key: format!("Object key: {key}"),
+                msg: format!("SUCCESS: {:?}", resp_msg),
+            });
+        }
+        _ => {
+            println!("Event type not supported: {:?}", event_type);
+            return Ok(Response {
+                event_type: format!("Event type not supported: {event_type}"),
+                key: format!("Event type not supported for key: {key}"),
+                msg: format!("ERROR for {event_type}"),
+            });
+        }
+    }
 }
 
 #[tokio::main]
